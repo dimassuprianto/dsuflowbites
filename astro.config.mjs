3203bdaab563ce760694c8362ad06f07a6e14445
Dimas Suprianto <pc1000.dimas@gmail.com>
2025-11-19 20:41:06 +0700

    cek CI

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 const DEV_PORT = 2121;
-const LIVE_URL = 'https://dimassuprianto.github.io/dsuflowbites'
+//const LIVE_URL = 'https://dimassuprianto.github.io/dsuflowbites'
 
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.CI
-		? { 'https://dimassuprianto.github.io/', base: '/dsuflowbites' }
+		? /*{*/
+			'https://dimassuprianto.github.io/dsuflowbites'/*, base: '/dsuflowbites' }*/
 		: `http://localhost:${DEV_PORT}`,
 	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
 
